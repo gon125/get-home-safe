bcd05bae7464512a396195e5f4dbef0aa7e7dfbd
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-03-31 16:41:05 +0900

    [#2] Create project-setup.sh

---
+++ scripts/project-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Script to configure SwiftLint + GitHooks
+
+# Directories
+GIT_ROOT=$(git rev-parse --show-toplevel)
+SCRIPTS_DIRECTORY=$GIT_ROOT/scripts
+GIT_HOOKS_DIRECTORY=$SCRIPTS_DIRECTORY/git-hooks
+
+# -- 1 -- Linking `.swiftLint.yml`
+echo "1. Linking .swiftlint.yml:"
+ln -sf $SCRIPTS_DIRECTORY/.swiftlint.yml $GIT_ROOT/swiftlint.yml 
+
+if [[ $? != 0 ]] ; then
+echo ".swiftlint.yml moving failed"
+else 
+echo ".swiftlint.yml moved successfully"
+fi
+
+# -- 2 -- Reassigning hooks
+echo "2. Reassigning Git Hooks:"
+git config core.hooksPath $GIT_HOOKS_DIRECTORY
+chmod +x $GIT_HOOKS_DIRECTORY/*
+echo "Hooks successfully reassigned to '$GIT_HOOKS_DIRECTORY'"

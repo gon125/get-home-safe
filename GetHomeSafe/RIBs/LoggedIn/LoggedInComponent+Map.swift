a6b92af967e48462069f3f97ff9c7de0e31f0fbe
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-05-11 22:34:37 +0900

    [#30] Create LoggedInComponent+Map.swift

---
+++ GetHomeSafe/RIBs/LoggedIn/LoggedInComponent+Map.swift
@@ -0,0 +1,19 @@
+//
+//  LoggedInComponent+Map.swift
+//  GetHomeSafe
+//
+//  Created by Geonhyeong LIm on 2021/05/11.
+//
+
+import RIBs
+
+// TODO: Update LoggedInDependency protocol to inherit this protocol.
+protocol LoggedInDependencyMap: Dependency {
+    // TODO: Declare dependencies needed from the parent scope of LoggedIn to provide dependencies
+    // for the Map scope.
+}
+
+extension LoggedInComponent: MapDependency {
+
+    // TODO: Implement properties to provide for Map scope.
+}

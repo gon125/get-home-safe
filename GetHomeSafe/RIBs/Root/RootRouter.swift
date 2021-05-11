8346d32d622907f575475c97407d6bbdaa95664f
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-05-11 22:39:33 +0900

    [#30] Update RootRouter.swift

--- GetHomeSafe/RIBs/Root/RootRouter.swift
+++ GetHomeSafe/RIBs/Root/RootRouter.swift
@@ -7,7 +7,7 @@
 
 import RIBs
 
-protocol RootInteractable: Interactable, LoggedOutListener {
+protocol RootInteractable: Interactable, LoggedOutListener, LoggedInListener {
     var router: RootRouting? { get set }
     var listener: RootListener? { get set }
 }
@@ -19,7 +19,14 @@ protocol RootViewControllable: ViewControllable {
 
 final class RootRouter: LaunchRouter<RootInteractable, RootViewControllable>, RootRouting {
     func routeToLoggedIn() {
-        //
+        if let loggedOut = self.loggedOut {
+            detachChild(loggedOut)
+            viewController.dismiss(viewController: loggedOut.viewControllable)
+            self.loggedOut = nil
+        }
+        
+        let loggedIn = loggedInBuilder.build(withListener: interactor)
+        attachChild(loggedIn)
     }
 
     init(interactor: RootInteractable,

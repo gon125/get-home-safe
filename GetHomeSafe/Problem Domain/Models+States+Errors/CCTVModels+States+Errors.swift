d6ea57280215d03c889c52645c54786d4593761c
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-05-12 14:41:57 +0900

    [#37] Create CCTVModels+States+Errors.swift

---
+++ GetHomeSafe/Problem Domain/Models+States+Errors/CCTVModels+States+Errors.swift
@@ -0,0 +1,12 @@
+//
+//  CCTVModels+States+Errors.swift
+//  GetHomeSafe
+//
+//  Created by Geonhyeong LIm on 2021/05/12.
+//
+
+import Foundation
+
+struct CCTV: Location {
+    let coordinate: Coordinate2D
+}

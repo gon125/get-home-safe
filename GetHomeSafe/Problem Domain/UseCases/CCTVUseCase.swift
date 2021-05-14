efa91c6b297e0ce84438eef1c78d2cfcb1ec9c41
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-05-14 15:38:54 +0900

    [#37] Update CCTVUseCase.swift

--- GetHomeSafe/Problem Domain/UseCases/CCTVUseCase.swift
+++ GetHomeSafe/Problem Domain/UseCases/CCTVUseCase.swift
@@ -15,7 +15,15 @@ protocol CCTVUseCase {
 #if DEBUG
 struct StubCCTVUseCase: CCTVUseCase {
     func getCCTVs(near currentLocation: Location) -> AnyPublisher<[CCTV], Never> {
-        let cctvs: [CCTV] = [.init(coordinate: currentLocation.coordinate)]
+        var coordnates = [Coordinate2D]()
+        for _ in 0..<10 {
+            coordnates.append(
+                Coordinate2D(
+                    latitude: currentLocation.coordinate.latitude + Double.random(in: -0.006..<0.006),
+                    longitude: currentLocation.coordinate.longitude + Double.random(in: -0.006..<0.006))
+            )
+        }
+        let cctvs: [CCTV] = coordnates.map { CCTV(coordinate: $0) }
         return Just(cctvs).eraseToAnyPublisher()
     }
 }

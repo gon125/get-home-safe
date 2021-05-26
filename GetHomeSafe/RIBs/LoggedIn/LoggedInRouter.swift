5aa4b1efe0805385c604f3bf0139ba380ae3b9e1
KHyoseon <apso123@naver.com>
2021-05-26 18:42:03 +0900

    [#45] loggedIn Rib
    Edit Router, Builder

--- GetHomeSafe/RIBs/LoggedIn/LoggedInRouter.swift
+++ GetHomeSafe/RIBs/LoggedIn/LoggedInRouter.swift
@@ -15,46 +15,49 @@ protocol LoggedInInteractable: Interactable, MapListener {
 protocol LoggedInViewControllable: ViewControllable {
     func present(viewController: ViewControllable)
     func dismiss(viewController: ViewControllable)
-    // TODO: Declare methods the router invokes to manipulate the view hierarchy. Since
-    // this RIB does not own its own view, this protocol is conformed to by one of this
-    // RIB's ancestor RIBs' view.
 }
 
 final class LoggedInRouter: Router<LoggedInInteractable>, LoggedInRouting {
-    
-    // MARK: - LoggedInRouting
-    func routeToMap() {
-        if let currentChild = currentChild {
-            viewController.dismiss(viewController: currentChild.viewControllable)
-            detachChild(currentChild)
-        }
-        attachMap()
-    }
 
     // TODO: Constructor inject child builder protocols to allow building children.
-    init(interactor: LoggedInInteractable,
-         viewController: LoggedInViewControllable,
-         mapBuilder: MapBuildable) {
+    init(interactor: LoggedInInteractable, viewController: LoggedInViewControllable, mapBuilder: MapBuildable) {
         self.viewController = viewController
         self.mapBuilder = mapBuilder
         super.init(interactor: interactor)
         interactor.router = self
     }
 
-    func cleanupViews() {
-        // TODO: Since this router does not own its view, it needs to cleanup the views
-        // it may have added to the view hierarchy, when its interactor is deactivated.
+    override func didLoad() {
+        super.didLoad()
+        attachMap()
     }
 
-    // MARK: - Private
-    private let mapBuilder: MapBuildable
-    private let viewController: LoggedInViewControllable
+    func cleanupViews() {
+        if let currentChild = currentChild {
+            viewController.dismiss(viewController: currentChild.viewControllable)
+        }
+    }
+
+    func routeToMap() {
+        detachCurrentChild()
+        attachMap()
+    }
+
     private var currentChild: ViewableRouting?
-    
+    private let viewController: LoggedInViewControllable
+    private let mapBuilder: MapBuildable
+
     private func attachMap() {
         let map = mapBuilder.build(withListener: interactor)
         self.currentChild = map
         attachChild(map)
         viewController.present(viewController: map.viewControllable)
+    }
+
+    private func detachCurrentChild() {
+        if let currentChild = currentChild {
+            detachChild(currentChild)
+            viewController.dismiss(viewController: currentChild.viewControllable)
+        }
     }
 }

f016cd5af78cf80d4e094edf1f5f10e904e402d9
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-06-07 16:07:40 +0900

    [#47] Update LoggedOutViewController.swift

--- GetHomeSafe/RIBs/LoggedOut/LoggedOutViewController.swift
+++ GetHomeSafe/RIBs/LoggedOut/LoggedOutViewController.swift
@@ -164,22 +164,3 @@ extension LoggedOutViewController: UITextFieldDelegate {
         return true
     }
 }
-
-class TextFieldWithPadding: UITextField {
-    var textPadding = UIEdgeInsets(
-        top: 10,
-        left: 12,
-        bottom: 10,
-        right: 12
-    )
-
-    override func textRect(forBounds bounds: CGRect) -> CGRect {
-        let rect = super.textRect(forBounds: bounds)
-        return rect.inset(by: textPadding)
-    }
-
-    override func editingRect(forBounds bounds: CGRect) -> CGRect {
-        let rect = super.editingRect(forBounds: bounds)
-        return rect.inset(by: textPadding)
-    }
-}
